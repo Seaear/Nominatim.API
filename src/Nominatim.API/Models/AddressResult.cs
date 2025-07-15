0797733fa48a61bbf21faafe4085541ab69b9948
SNS <sns@kmnet.net>
2025-07-15 19:46:56 +0800

    增强地址信息支持，添加新属性
    在 `AddressResult.cs` 文件中，添加了多个新属性以支持更详细的地址信息，包括 `Pedestrian`（步行）、`Residential`（住宅区）、`Amenity`（设施）、`Military`（军事区）和 `Building`（建筑）。同时，更新了部分属性的注释以提供更清晰的描述，并移除了部分原有的注释内容，从而增强了代码的可读性和国际化支持。

--- src/Nominatim.API/Models/AddressResult.cs
+++ src/Nominatim.API/Models/AddressResult.cs
@@ -9,13 +9,13 @@ namespace Nominatim.API.Models {
         public string Country { get; set; }
 
         /// <summary>
-        ///     Country code
+        ///     Country code 
         /// </summary>
         [JsonProperty("country_code")]
         public string CountryCode { get; set; }
 
         /// <summary>
-        ///     County name
+        ///     County name 县
         /// </summary>
         [JsonProperty("county")]
         public string County { get; set; }
@@ -45,22 +45,25 @@ namespace Nominatim.API.Models {
         public string State { get; set; }
 
         /// <summary>
-        ///     Town Name
+        ///     Town Name 城镇
         /// </summary>
         [JsonProperty("town")]
         public string Town { get; set; }
 
+        /// <summary>
+        ///     Pedestrian 步行
+        /// </summary>
         [JsonProperty("pedestrian")]
         public string Pedestrian { get; set; }
 
         /// <summary>
-        ///     Neighbourhood
+        ///     Neighbourhood 街区
         /// </summary>
         [JsonProperty("neighbourhood")]
         public string Neighborhood { get; set; }
 
         /// <summary>
-        ///     Hamlet
+        ///     Hamlet 村庄
         /// </summary>
         [JsonProperty("hamlet")]
         public string Hamlet { get; set; }
@@ -72,7 +75,7 @@ namespace Nominatim.API.Models {
         public string Suburb { get; set; }
 
         /// <summary>
-        ///     Village Name
+        ///     Village Name 村庄
         /// </summary>
         [JsonProperty("village")]
         public string Village { get; set; }
@@ -90,7 +93,7 @@ namespace Nominatim.API.Models {
         public string Region { get; set; }
 
         /// <summary>
-        ///     District Name
+        ///     District Name 区
         /// </summary>
         [JsonProperty("district")]
         public string District { get; set; }
@@ -102,21 +105,45 @@ namespace Nominatim.API.Models {
         public string Name { get; set; }
 
         /// <summary>
-        ///     Tourism
+        ///     Tourism 旅游区
         /// </summary>
         [JsonProperty("tourism")]
         public string Tourism { get; set; }
 
         /// <summary>
-        ///     Municipality
+        ///     Municipality 市
         /// </summary>
         [JsonProperty("municipality")]
         public string Municipality { get; set; }
 
         /// <summary>
-        ///     Quarter
+        ///     Quarter 街区
         /// </summary>
         [JsonProperty("quarter")]
         public string Quarter { get; set; }
+
+        /// <summary>
+        ///     Residential 住宅区
+        /// </summary>
+        [JsonProperty("residential")]
+        public string Residential { get; set; }
+
+        /// <summary>
+        ///     Amenity 设施
+        /// </summary>
+        [JsonProperty("amenity")]
+        public string Amenity { get; set; }
+
+        /// <summary>
+        ///     Military 军事区
+        /// </summary>
+        [JsonProperty("military")]
+        public string Military { get; set; }
+
+        /// <summary>
+        ///     Building 建筑
+        /// </summary>
+        [JsonProperty("building")]
+        public string Building { get; set; }
     }
 }
